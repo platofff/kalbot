1f961ae17d6c07f92d5fa3b39ce73fb86e4398c4
platofff <42435266+platofff@users.noreply.github.com>
2020-08-08 00:05:49 +0300

    Update searchimages.py

--- images/searchimages.py
+++ images/searchimages.py
@@ -1,16 +1,28 @@
 import requests
 import json
 
+SERVERS = [
+"https://searx.xyz",
+"https://searx.privatenet.cf",
+"https://searx.gnu.style",
+"https://searx.be",
+"https://searx.nevrlands.de"
+]
+
 class ImgSearch:
     def fetch(self, query):
         result = []
-        r = requests.get('https://searx.fmac.xyz', params=
-        {"q": query,
-         "categories": "images",
-         "format": "json"
-         })
-        print(r.text)
-        results = json.loads(r.text)['results']
+        results = []
+        for server in SERVERS:
+            try:
+                r = requests.get(server, params=
+                    {"q": query,
+                    "categories": "images",
+                    "format": "json"
+                })
+                results = json.loads(r.text)['results']
+            except JSONDecodeError:
+                continue
         for r in results:
             if r['img_src'][-4:] in ['.gif', '.jpg', '.png']:
                 if r['img_src'][:4] != "http":

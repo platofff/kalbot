094595a8e1ace24f3b1aac9438617b5bf53d4b6f
platofff <42435266+platofff@users.noreply.github.com>
2020-08-07 23:47:22 +0300

    Update searchimages.py

--- images/searchimages.py
+++ images/searchimages.py
@@ -1,19 +1,15 @@
-import urllib3
+import requests
 import json
 
 class ImgSearch:
-
-    def __init__(self):
-        self.http = urllib3.PoolManager()
-
     def fetch(self, query):
         result = []
-        r = self.http.request('GET', 'https://searx.fmac.xyz', fields=
+        r = requests.get('https://searx.fmac.xyz', params=
         {"q": query,
          "categories": "images",
          "format": "json"
          })
-        results = json.loads(r.data)['results']
+        results = json.loads(r.text)['results']
         for r in results:
             if r['img_src'][-4:] in ['.gif', '.jpg', '.png']:
                 if r['img_src'][:4] != "http":

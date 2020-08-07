8ca4fdf405ce6b4f67502d28c7538395ae894bae
platofff <42435266+platofff@users.noreply.github.com>
2020-08-07 19:05:17 +0300

    Update searchimages.py

--- images/searchimages.py
+++ images/searchimages.py
@@ -9,7 +9,10 @@ class ImgSearch:
 
     def __init__(self):
         with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'googleapi.yaml')) as c:
-            config = yaml.safe_load(c)
+            if not (os.environ['GAPI_KEY'] and os.environ['GPROJECT_CX']):
+                config = yaml.safe_load(c)
+            else:
+                config = { 'dev_api_key': os.environ['GAPI_KEY'], 'project_cx': os.environ['GPROJECT_CX'] }
             self._gis = GoogleImagesSearch(config['dev_api_key'], config['project_cx'], validate_images=True)
 
     def fetch(self, query, count):

1040003332018a6b39c777c56e17443997a8c82d
platofff <platofff@github.com>
2020-08-08 12:40:04 +0300

    Debug

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ class Bot:
         class Demotivator:
             @staticmethod
             async def run(event: BotEvent):
-                return f"{dir(event.object.object.message.attachments)} {event.object.object.message.attachments}"
+                return f"{event.object.object.message.attachments[0].url}"
 
     class _TextFilters:
         filters = []

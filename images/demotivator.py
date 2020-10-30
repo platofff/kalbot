1f16688e4f82455eb99b4cd10b748e938522e8f6
platofff <42435266+platofff@users.noreply.github.com>
2020-10-30 17:01:49 +0200

    Update demotivator.py

--- images/demotivator.py
+++ images/demotivator.py
@@ -12,34 +12,33 @@ from PIL import Image, ImageDraw, ImageFont
 class Demotivator:
     pattern: Image
     background: Image
-    MAX_LEN_BIG = 25
-    MAX_LEN_SM = 40
-
-    def __init__(self):
-        self.pattern = Image.open(os.path.join(sys.path[0], "images", "demotivator.jpg"))
-        self.font1 = ImageFont.truetype(font=os.path.join(sys.path[0], "images", "fonts", "LiberationSerif-TWEmoji.ttf"),
-                                        size=48, encoding="unic")
-        self.font2 = ImageFont.truetype(font=os.path.join(sys.path[0], "images", "fonts", "LiberationSans-TWEmoji.ttf"),
-                                        size=28, encoding="unic")
+    BIG_FONT_DIV = 6
+    SM_FONT_DIV = 10
 
     def create(self, url, text1, text2, name=None):
         if not name:
             name = str(randint(-32767, 32767)) + '.png'
         r = request.urlopen(url)
         img = Image.open(r)
-        img = img.resize((542, 358))
-        result = self.pattern.copy()
-        result.paste(img, (58, 58))
 
-        def addBlack(num, img):
-            self.background = Image.new('RGB', (img.size[0], img.size[1] + num * 50), (0, 0, 0))
+        font1 = ImageFont.truetype(font=os.path.join(sys.path[0], "images", "fonts", "LiberationSerif-TWEmoji.ttf"),
+                                   size=ceil(img.size[1] / self.BIG_FONT_DIV), encoding="unic")
+        font2 = ImageFont.truetype(font=os.path.join(sys.path[0], "images", "fonts", "LiberationSans-TWEmoji.ttf"),
+                                   size=ceil(img.size[1] / self.SM_FONT_DIV), encoding="unic")
+
+        result = Image.new('RGB', (ceil(img.size[0] * 1.2), ceil(img.size[1] * 1.4)))
+        draw = ImageDraw.Draw(result)
+        offset = ceil(img.size[0] * 0.1)
+        result.paste(img, (offset, offset))
+        draw.rectangle([offset - 6, offset - 6, result.size[0] - offset + 6, img.size[1] + offset + 6], fill=None,
+                       outline=(255, 255, 255), width=3)
+
+        def addBlack(px, img):
+            self.background = Image.new('RGB', (img.size[0], img.size[1] + px), (0, 0, 0))
             self.background.paste(img, (0, 0))
             return self.background.copy()
 
-        # text1 = '\n'.join([textwrap.fill(x, self.MAX_LEN_BIG) for x in text1.split('\n')])
-        # text2 = '\n'.join([textwrap.fill(x, self.MAX_LEN_SM) for x in text2.split('\n')])
-
-        draw = ImageDraw.Draw(result)
+        max_w = ceil(result.size[0] * 0.8)
 
         def formatText(text, font):
             text = text.split('\n').reverse() or [text]
@@ -47,7 +46,7 @@ class Demotivator:
             for i in range(_l):
                 while True:
                     w, h = draw.textsize(text[i], font=font)
-                    if w < 630:
+                    if w < max_w:
                         break
                     else:
                         splitted = text[i].split(' ')
@@ -62,22 +61,23 @@ class Demotivator:
             else:
                 return formatText('\n'.join(list(text.reverse() or text)), font)
 
-        text1 = formatText(text1, self.font1).lstrip()
-        text2 = formatText(text2, self.font2).lstrip()
+        text1 = formatText(text1, font1).lstrip()
+        text2 = formatText(text2, font2).lstrip()
 
-        w1, h1 = draw.textsize(text1, font=self.font1)
-        w2, h2 = draw.textsize(text2, font=self.font2)
+        w1, h1 = draw.textsize(text1, font=font1)
+        w2, h2 = draw.textsize(text2, font=font2)
+        cur_text_h = result.size[1] - offset - font1.size - img.size[1]
 
-        blackNum = 0
-        if h1 + h2 > 140:
-            blackNum += ceil((h1 + h2 - 140) / 50)
-        result = addBlack(blackNum, result)
-        del blackNum
+        if h1 + h2 > cur_text_h:
+            black_px = h1 + h2 - cur_text_h
+            result = addBlack(black_px, result)
+            draw = ImageDraw.Draw(result)
 
-        draw = ImageDraw.Draw(result)
-        draw.multiline_text(((655 - w1) / 2, 450), text1, fill="white", font=self.font1, align="center")
-        draw.multiline_text(((655 - w2) / 2, 470 + h1), text2, fill="white", font=self.font2, align="center")
+        draw.multiline_text(((result.size[0] - w1) / 2, img.size[1] + offset + ceil(font2.size / 2)), text1,
+                            fill="white", font=font1, align="center")
+        draw.multiline_text(((result.size[0] - w2) / 2, ceil(font1.size / 2) + img.size[1] + h1 + offset), text2,
+                            fill="white", font=font2, align="center")
 
-        fPath = os.path.join(tempfile.gettempdir(), name)
-        result.save(fPath)
-        return fPath
+        f_path = os.path.join(tempfile.gettempdir(), name)
+        result.save(f_path)
+        return f_path

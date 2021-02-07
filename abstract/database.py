0f52db36defb570b2724ce83fc62bb41477485d9
platofff <42435266+platofff@users.noreply.github.com>
2021-02-07 11:04:56 +0200

    Upgrade dependencies

--- abstract/database.py
+++ abstract/database.py
@@ -12,12 +12,12 @@ class Database:
     def __init__(self):
         if 'DATABASE_URL' in environ:
             config = urlparse(environ['DATABASE_URL'])
-            self.con = pymysql.Connect(config.hostname, config.username, config.password, config.path.lstrip('/'),
-                                       autocommit=True)
+            self.con = pymysql.Connect(host=config.hostname, user=config.username, password=config.password,
+                                       database=config.path.lstrip('/'), autocommit=True)
         else:
             with open('mysql.yaml') as c:
                 config = yaml.safe_load(c)
-            self.con = pymysql.Connect(config['host'], config['username'], config['password'], config['db'],
-                                       autocommit=True)
+            self.con = pymysql.Connect(host=config['host'], user=config['username'], password=config['password'],
+                                       database=config['db'], autocommit=True)
 
         atexit.register(lambda: self.con.close())
